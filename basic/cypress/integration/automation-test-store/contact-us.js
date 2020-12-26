e5f9829fc580411cb88bd74a60b629b7e050ee4a
Leonid M <eleoneks@gmail.com>
2020-12-26 14:46:16 +0200

    update lessons

--- basic/cypress/integration/automation-test-store/contact-us.js
+++ basic/cypress/integration/automation-test-store/contact-us.js
@@ -3,7 +3,9 @@
 describe("Test Contact Us form via Automation Test Store", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
       cy.visit("https://www.automationteststore.com/");
-      cy.get("a[href$='contact']").click();
+      cy.get("a[href$='contact']").click().then(function(linkText) {
+        cy.log("Clicked on link using text: " + linkText.text())
+      });
       //cy.xpath("//a[contains(@href, 'contact')]").click();
       cy.get('#ContactUsFrm_first_name').type("Joe");
       cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
