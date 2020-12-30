397fddb17b15dac8020a49964e338d34c373425c
AZANIR <eleoneks@gmail.com>
2020-12-30 23:37:29 +0200

    Add drive project

--- basic/cypress/integration/webdriver-uni/radio-buttons.js
+++ basic/cypress/integration/webdriver-uni/radio-buttons.js
@@ -2,7 +2,7 @@
 
 describe("Verify radio buttons via webdriveruni", () => {
     before(function () {
-        cy.visit("http://www.webdriveruniversity.com")
+        cy.visit("/")
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({
             force: true
         })
